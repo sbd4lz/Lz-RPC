78f96ef3f8b8fc2934bced28ae7c1f625ecead03
顶级靓仔 <9469309+dengpei1@user.noreply.gitee.com>
2024-10-29 15:20:18 +0800

    胎儿

---
+++ src/main/java/com/liangzai/lzrpc/LzRpcApplication.java
@@ -0,0 +1,13 @@
+package com.liangzai.lzrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LzRpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LzRpcApplication.class, args);
+	}
+
+}
